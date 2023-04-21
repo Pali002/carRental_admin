4688c8959aa98d974d6a655701a665c7bc4c94f3
Zentai Pál <zentaipali@gmail.com>
2023-04-21 09:58:04 +0200

    hozzáadás ablak

--- src/views/MainView.java
+++ src/views/MainView.java
@@ -2,13 +2,22 @@ package views;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.IntegerStringConverter;
 import models.Car;
@@ -26,13 +35,6 @@ public class MainView extends VBox {
     // InputPanel nameInput;
 
     private TableView<Car> tableView;
-    // private TextField nameInput,
-    // brandInput,
-    // platenumberInput,
-    // quantityInput,
-    // spaceInput,
-    // gearboxInput,
-    // priceInput;
 
     public MainView() {
         carsLabel = new Label("Autók");
@@ -164,6 +166,83 @@ public class MainView extends VBox {
         this.addButton = new Button();
         this.addButton.setText("Hozzáadás");
         this.getChildren().add(this.addButton);
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                GridPane grid = new GridPane();
+
+                grid.setAlignment(Pos.CENTER);
+                grid.setHgap(10);
+                grid.setVgap(10);
+                grid.setPadding(new Insets(25, 25, 25, 25));
+
+                // Add labels and text fields for make, model, year, and color
+                Label nameLabel = new Label("Név:");
+                grid.add(nameLabel, 0, 0);
+                TextField nameField = new TextField();
+                grid.add(nameField, 1, 0);
+
+                Label brandLabel = new Label("Márka:");
+                grid.add(brandLabel, 0, 1);
+                TextField brandField = new TextField();
+                grid.add(brandField, 1, 1);
+
+                Label platenumberLabel = new Label("Rendszám:");
+                grid.add(platenumberLabel, 0, 2);
+                TextField platenumberField = new TextField();
+                grid.add(platenumberField, 1, 2);
+
+                Label quantityLabel = new Label("Mennyiség:");
+                grid.add(quantityLabel, 0, 3);
+                TextField quantityField = new TextField();
+                grid.add(quantityField, 1, 3);
+
+                Label spaceLabel = new Label("Férőhely:");
+                grid.add(spaceLabel, 0, 4);
+                TextField spaceField = new TextField();
+                grid.add(spaceField, 1, 4);
+
+                Label gearboxLabel = new Label("Váltó:");
+                grid.add(gearboxLabel, 0, 5);
+                TextField gearboxField = new TextField();
+                grid.add(gearboxField, 1, 5);
+
+                Label priceLabel = new Label("Ár:");
+                grid.add(priceLabel, 0, 6);
+                TextField priceField = new TextField();
+                grid.add(priceField, 1, 6);
+
+                // HBox, mentés és mezők űrítése gomb
+                HBox hbBtn = new HBox(10);
+                hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+                Button submitButton = new Button("Submit");
+                Button clearButton = new Button("Clear");
+                hbBtn.getChildren().addAll(submitButton, clearButton);
+                grid.add(hbBtn, 1, 7);
+
+                Stage addStage = new Stage();
+                Scene addScene = new Scene(grid, 300, 350);
+                addStage.setTitle("Új autó felvétele");
+                addStage.setScene(addScene);
+                addStage.show();
+
+                // mentés gomb
+                // submitButton.setOnAction(event -> {
+
+                // });
+
+                // mezők ürítése gomb
+                clearButton.setOnAction(event -> {
+                    nameField.setText("");
+                    brandField.setText("");
+                    platenumberField.setText("");
+                    quantityField.setText("");
+                    spaceField.setText("");
+                    gearboxField.setText("");
+                    priceField.setText("");
+                });
+            }
+        });
 
         // torles gomb
         this.deleteButton = new Button();
